e509e43184a038540ed781712041145633f7de4c
이찬호 <2018009870@hanyang.ac.kr>
2021-03-18 11:16:03 +0900

    Using List for data processing

--- project1/apriori.py
+++ project1/apriori.py
@@ -2,47 +2,50 @@ import os
 import sys
 import math
 import numpy as np
-import pandas as pd
 import time
 
 path = os.getcwd()
 
 def frequent(data,itemset):
   # itemset is dictionary and data is 2d-array, key of dictionary is tuple
-  for i in list(itemset.keys()):
-    count = 0
-    for j in data:
-      if set(i).issubset(set(j)):
-        count += 1
-    itemset[i] = count
+  # count the number of element in data
+  for i in data:
+    for j in itemset.keys():
+      if set(j).issubset(set(i)):
+        itemset[j] += 1 
   return itemset
   
-def generate_candidate(itemset,MinimumSupport,numberofdata):
+def generate_candidate(itemset):
+    # using apriori theroem
     key = list(itemset.keys())
     new_key = dict()
     past_length = len(key[0])
     now_length = past_length + 1
     low_key = list()
-    for i in key:
-        if itemset[i]/numberofdata*100 < MinimumSupport:
-            low_key.append(i)
-
+    # self joining
     for i in range(len(key)):
         for j in range(len(key)):
             flag = 0
+            if i == j:
+                break
             candidate = set(key[i]).union(set(key[j]))
             candidate = list(candidate)
             candidate.sort()
-            for k in low_key:
-                if set(k) in candidate:
-                    flag = 1
-                    break
+            candidate_element = list()
+            # pruncing
+            for k in range(len(candidate)):
+                temp = candidate.copy()
+                del temp[k]
+                temp = tuple(temp)
+                candidate_element.append(temp)
+
+            if not set(candidate_element).issubset(set(key)):
+                flag = 1
             if len(candidate) == now_length and flag == 0:
                 new_key[tuple(candidate)] = 0
-
     return new_key
 
-def powerset(itemset):
+def subset(itemset):
     element = list(itemset)
     count = int(math.pow(2,len(itemset)) - 1)
     answer = list()
@@ -57,12 +60,14 @@ def powerset(itemset):
         answer.append(subset)
     return answer
 
-    
-command = sys.argv = "apriori.py 5 input.txt ouput.txt".split()
+
+command = sys.argv
 
 try :
     if len(command) != 4:
         raise Exception("명령어가 잘못 입력되었습니다.")
+    start = time.time()
+    print("명령어가 입력되었습니다 support가 "+command[1]+"%로 진행됩니다.")
 except Exception as e:
     print("명령어를 문법에 맞게 사용하여 주세요. ex. apriori.py 5 input.txt output.txt")
     exit()
@@ -76,6 +81,7 @@ OutputData = open(path+'/'+OutputFileName,'w')
 
 ## Data 읽어오기
 RawData = list()
+
 while 1:
     transaction = InputData.readline().replace("\n","").split('\t')
     if transaction == [''] :
@@ -83,21 +89,18 @@ while 1:
     transaction = list(map(int, transaction)) ## str2int in list
     RawData.append(transaction)
 
-    ## remove ID
-data = pd.DataFrame(RawData)
-items = data.iloc[:,[1,2,3,4,5,6,7,8,9,10,11]]
-items = items.dropna(axis=0,how='all')
-## make 1-item frequent
-items = items.values.tolist()
+
 itemset = dict()
-numberofdata = len(items)
-for i in items:
+numberofdata = len(RawData)
+for i in RawData:
     for j in i:
         if not itemset.get((j,),0):
             itemset[(j,)] = 0
 
+print(time.time()-start)
+
 # Check frequent 1-itemset
-itemset = frequent(items,itemset)
+itemset = frequent(RawData,itemset)
 low_key = list()
 for i in itemset.keys():
     if itemset[i]/numberofdata*100 < MinimumSupport:
@@ -107,29 +110,27 @@ for i in low_key:
 
 # Generate candidate itemsets of length (k+1) from frequent itemsets of length k ==> 기존의 데이터를 넣어주면, 이를 활용해서 새로운 데이터들을 hash table에 추가하는 형식으로
 new_itemset = itemset
+
 while True:
   # generate candidate
-    new_itemset = generate_candidate(new_itemset,MinimumSupport,numberofdata)
+    new_itemset = generate_candidate(new_itemset)
   # check candidate
-    new_itemset = frequent(items,new_itemset)
+    new_itemset = frequent(RawData,new_itemset)
     low_key = list()
     for i in new_itemset.keys():
         if new_itemset[i]/numberofdata*100 < MinimumSupport:
             low_key.append(i)
-    L = new_itemset
     for i in low_key:
-        del L[i]
-
+        del new_itemset[i]
+    print(time.time()-start)
   # if result is NULL, stop the generate candidate
-    if L == dict():
+    if new_itemset == dict():
         break
     else :
-        itemset.update(L)
-
+        itemset.update(new_itemset)
 # calculate support and confidence
-
 for i in itemset.keys():
-    element = powerset(i)
+    element = subset(i)
     for j in element:
         if len(i) == 1:
             before = i
@@ -152,6 +153,3 @@ for i in itemset.keys():
 
 OutputData.close()
 InputData.close()
-        
-        
-
