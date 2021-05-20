fbf44e44e638245a1023386cdd5bde5420c657ea
이찬호 <dlcksgh3199@naver.com>
2021-05-20 21:35:32 +0900

    Create recommender.py

---
+++ longTermProject/recommender.py
@@ -0,0 +1,14 @@
+import numpy as np
+import pandas as pd
+
+# read data
+
+# convert raw data to rating matrix (post-use matrix)
+
+# make pre-use matrix ( 0 - 1 rating matrix and fill in the blank use WRMF method ) // WRMF 조사
+
+# conversion pre-use matrix to post-use matrix by specific method // pre-use matrix를 어떻게 반영할 것인지에 대해서 조사
+
+# predicting the data using SVD method // SVD를 어떻게 진행하는지 조사
+
+# save the data to txt file format
